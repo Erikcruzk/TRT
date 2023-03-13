3ea1ac1c23b5ec969b1f6ad9b4452185e0f7c30d
Erik Cruz <erik.cruz@smsteknik.se>
2023-03-13 05:50:42 +0100

    Changes:
    - Now loops directories in parallell to create the final graph
    - Added better size of graph and json dump the graph to reuse later

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from SmartContract import SmartContract
-from PromptEngine import PromptEngine
 from TransformativeRepair import TransformativeRepair
-import os
-
-
-def find_vulnerabilities_and_repair_sc_in_directory(directory_path):
-    for filename in os.listdir(directory_path):
-        find_vulnerablities_and_repair_sc(os.path.join(directory_path, filename))
-
-
 
 def main():
     sc_path = 'experiments/sc_to_repair/reentrance.sol'
+    sc_directory = 'experiments/sc_to_repair'
 
     # 0. Initialize TFR!
     tfr = TransformativeRepair()
 
     # 1. Activate to show the different prompt_styles
-    tfr.show_prompt_types(sc_path)
+    # tfr.show_prompt_types(sc_path)
 
     # 2. Activate to Generate a simple Network Graph of a simple repair
-    tfr.create_repair_results_network(sc_path, promt_style='C_vulnerability_examples', vulnerability_limitations=['reentrancy'], temperature=0.5, top_p=0.95, n_repairs=2)
-    tfr.visualize_graph_pyvis('results/test1')
+    # G = tfr.create_repair_results_network(sc_path, promt_style='C_vulnerability_examples', vulnerability_limitations=['reentrancy'], temperature=0.5, top_p=0.95, n_repairs=2)
+    # tfr.visualize_graph_pyvis(G, 'results/simple_example')
+
+    # 3. Activate to Generate Network Grah of repairs of directory
+    G = tfr.find_vulnerabilities_and_repair_sc_in_directory(sc_directory, prompt_style='C_vulnerability_examples', vulnerability_limitations=['reentrancy'], temperature=0.5, top_p=0.95, n_repairs=2)
+    tfr.visualize_graph_pyvis(G, 'results/test_sb_curated_reentrancy')
 
 if __name__ == "__main__":
     main()
