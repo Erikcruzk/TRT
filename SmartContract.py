843f754c2f9fc652e992b4a4b581a23f78c9ea31
Erik Cruz <erik.cruz@smsteknik.se>
2023-03-27 16:03:43 +0200

    Large changes. Work in progress

--- SmartContract.py
+++ SmartContract.py
@@ -1,9 +1,12 @@
 import hashlib
 import logging
+from pathlib import Path
 import subprocess
 import json
 import os
 import traceback
+from typing import List
+import yaml
 
 class SmartContract:
     '''
@@ -26,22 +29,27 @@ class SmartContract:
     - get_vulnerabilities_difference(self, other_sc): compares the vulnerabilities detected in the current SmartContract instance to those detected in another SmartContract instance, and returns the differences.
     '''
 
-    def __init__(self, sc_path):
+    def __init__(self, experiment_settings:dict, sc_path:str, sc_results_dir:str):
         '''
         Constructor method that initializes a SmartContract object with the specified Solidity smart contract file path.
 
         Args:
         - sc_path (str): a string representing the file path to the Solidity smart contract.
         '''
+        self.experiment_settings = experiment_settings
         self.path = sc_path
         self.filename = os.path.basename(self.path)
+        self.name, _ = os.path.splitext(self.filename)
         self.language = 'Solidity' if self.filename.endswith('.sol') else 'Unknown'
         self.source_code = open(self.path, 'r').read()
         self.hash = hashlib.sha256(open(self.path, 'rb').read()).hexdigest()
         self.vulnerabilities = {}
         self.candidate_patches = []
+
+        self.results_dir = Path(os.path.join(sc_results_dir,
+                self.name))
     
-    def set_vulnerabilities(self, tool_name:str, tool_result:dict, vulnerability_limitations:list=[]):
+    def set_vulnerabilities(self, tool_name:str, tool_result:dict):
         """
         Set vulnerabilities for a given tool's result.
 
@@ -84,13 +92,12 @@ class SmartContract:
                     # Finally add vulnerability name
                     self.vulnerabilities[tool_name].append(vulnerability_name)
             # In strict mode clean undesired vulnerabilities
-            if vulnerability_limitations:
-                self.vulnerabilities[tool_name] = [x for x in self.vulnerabilities[tool_name] if x in vulnerability_limitations]
-
+            if self.experiment_settings["target_vulnerabilities"]:
+                self.vulnerabilities[tool_name] = [x for x in self.vulnerabilities[tool_name] if x in self.experiment_settings["target_vulnerabilities"]]            
         except Exception as e:
             logging.critical("An exception occurred: %s", str(e), exc_info=True)
 
-    def load_vulnerabilities(self, vulnerability_limitations:list=[]):
+    def get_vulnerabilities(self) -> None:
         '''
         Load the detected vulnerabilities from the results directory of the vulnerability detection tools.
 
@@ -102,32 +109,58 @@ class SmartContract:
 
         '''
         try:
-            results_directory = f'sb_results/{self.filename}'
-            if not os.path.isdir(results_directory):
-                raise FileNotFoundError(f'Results directory not found: {results_directory}')
-            for filename in os.listdir(results_directory):
-                full_path = os.path.join(results_directory, filename)
+            smartbugs_results_dir = os.path.join(self.results_dir, "smartbugs_results")
+
+            for smartbugs_result_file in os.listdir(smartbugs_results_dir):
+                full_path = os.path.join(smartbugs_results_dir, smartbugs_result_file)
                 if os.path.isdir(full_path):
                     tool_name = os.path.basename(full_path).split('_', 1)[0]
                     with open(os.path.join(full_path, 'result.json'), 'r') as f:
                         tool_result = json.load(f)
 
-                    self.set_vulnerabilities(tool_name, tool_result, vulnerability_limitations)
+                    self.set_vulnerabilities(tool_name, tool_result)
         except Exception as e:
             logging.critical("An exception occurred: %s", str(e), exc_info=True)
 
-    def find_vulnerabilities(self, vulnerability_limitations:list=[]):
+    def run_smartbugs(self) -> None:
         '''
         Use the Smart Bugs tool to find vulnerabilities in the smart contract.
 
         This method runs the Smart Bugs tool on the Solidity file and loads the resulting
         vulnerabilities into the `vulnerabilities` attribute using the `load_vulnerabilities` method.
         '''
-        try:
-            output = subprocess.run([f'./smartbugs/smartbugs -c smart_bugs_settings.yaml -f {self.path}'], capture_output=True, text=True, shell=True)
-            self.load_vulnerabilities(vulnerability_limitations)
-        except Exception as e:
-            logging.critical("An exception occurred: %s", str(e), exc_info=True)
+
+        smartbugs_results_dir = Path(os.path.join(self.results_dir,
+                "smartbugs_results"))
+            
+        #### Step 3: Find Vulnerabilities
+        # Create results directory
+        if smartbugs_results_dir.exists():
+            return # vulnerabilities already found TODO: check results file      
+        smartbugs_results_dir.mkdir(parents=True, exist_ok=True)
+
+        #### Create smartbugs config yaml file
+        # set the fields that are not editable
+        smartbugs_config = {
+            'runtime': False,
+            'runid': '${YEAR}${MONTH}${DAY}_${HOUR}${MIN}',
+            'json': True,
+            'tools': self.experiment_settings["smartbugs_tools"],
+            'results': os.path.join(smartbugs_results_dir, "${TOOL}_${RUNID}"),
+            'log': os.path.join(self.results_dir,'smartbugs_logs', '{RUNID}.log')
+        }
+
+        # write the data to a YAML file
+        smartbugs_config_path = os.path.join(self.results_dir,'smartbugs_config.yml')
+        with open(smartbugs_config_path, 'w') as f:
+            yaml.dump(smartbugs_config, f)
+
+        output = subprocess.run([f'./smartbugs/smartbugs -c {smartbugs_config_path} -f {self.path}'], capture_output=True, text=True, shell=True)
+        self.get_vulnerabilities()
+
+        # Save vulnerabilities
+        with open(os.path.join(self.results_dir, "vulnerabilities.json"), "w") as outfile:
+            outfile.write(json.dumps(self.vulnerabilities))
 
     def get_repaired_vulnerabilities(self, repaired_sc) -> dict:
         '''
