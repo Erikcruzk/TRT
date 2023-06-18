d31a842d016c2960a2a55585a6c7c72b950fc36e
Erik Cruz <erik.cruz@smsteknik.se>
2023-06-18 20:08:22 +0200

    Delete old experimets  and its contents

--- experiment_results/sbc_no_comment_1h_tools3_patches20_tmp0.7_topp0.95_gpt-3.5-turbo/unchecked_low_level_call/analyzers_natural_language_results/0x0cbe050f75bc8f8c2d6c0d249fea125fd6e1acc9/candidate_patches/patch_0/patch_0.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.10;
-
-contract Caller {
-    function callAddress(address a) {
-        require(a.call()); // Added require statement to check return value of external call
-    }
-}
--- experiment_results/sbc_no_comment_1h_tools3_patches20_tmp0.7_topp0.95_gpt-3.5-turbo/unchecked_low_level_call/analyzers_natural_language_results/0x0cbe050f75bc8f8c2d6c0d249fea125fd6e1acc9/candidate_patches/patch_17/patch_17.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.10;
-
-contract Caller {
-    function callAddress(address a) {
-        require(a.call()); // add require statement to check return value of call()
-    }
-}
